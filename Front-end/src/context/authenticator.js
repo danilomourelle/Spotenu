032c5705423b0f6049e88f67c3ff0095265bab07
Danilo Mourelle <danilomourelle@outlook.com>
2020-07-21 21:51:24 -0300

    contexts

---
+++ Front-end/src/context/authenticator.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const AuthenticatorContext = React.createContext()
+
+export default AuthenticatorContext
---
+++ Front-end/src/context/band.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const BandContext = React.createContext()
+
+export default BandContext
