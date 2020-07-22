116565ac10de11ac1d5bac32b88c92156159a0e4
Danilo Mourelle <danilomourelle@outlook.com>
2020-07-21 21:52:56 -0300

    retirando o redux

--- Front-end/src/containers/App/index.js
+++ Front-end/src/containers/App/index.js
@@ -1,23 +1,8 @@
 import React from "react";
 import { createGlobalStyle } from 'styled-components'
-// import { createStore, applyMiddleware, compose } from "redux";
-import { Provider } from "react-redux";
-// import { routerMiddleware } from "connected-react-router";
-// import thunk from "redux-thunk";
-// import { createBrowserHistory } from "history";
+
 import Router from '../../Router/router.js'
-// import { generateReducers } from "../../reducers";
 
-/* export const history = createBrowserHistory();
-
-const middlewares = [
-  applyMiddleware(routerMiddleware(history), thunk),
-  window.__REDUX_DEVTOOLS_EXTENSION__
-    ? window.__REDUX_DEVTOOLS_EXTENSION__()
-    : f => f
-];
-
-const store = createStore(generateReducers(history), compose(...middlewares)); */
 
 const GlobalStyle = createGlobalStyle`
   * {
