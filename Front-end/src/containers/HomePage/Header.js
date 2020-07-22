7f5091fb96383fcfeb7826fe7b11d0b2bf071b19
Danilo Mourelle <danilomourelle@outlook.com>
2020-07-22 15:56:44 -0300

    refact home page

--- Front-end/src/containers/HomePage/Header.js
+++ Front-end/src/containers/HomePage/Header.js
@@ -3,27 +3,15 @@ import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 import { BtnLogin } from '../../components/Buttons'
 import { routes } from '../../Router/router.js'
-
-const Wrapper = styled.div`
-  width: 100%;
-  min-width:800px;
-  height: 80px;
-  background-color: rgba(0,0,0,0.6);
-  padding: 40px  15px;
-  position: fixed;
-  display:flex;
-  align-items:center;
-  color:#fff;
-  justify-content:space-between;
-`
+import { HeaderWrapper } from './style'
 
 function Header() {
 
   return (
-    <Wrapper>
+    <HeaderWrapper>
       <p>PROJETO SPOTENU - DANILO MOURELLE - TURMA SAGAN</p>
       <Link to={routes.login}><BtnLogin size='small'>Entrar</BtnLogin></Link>
-    </Wrapper>
+    </HeaderWrapper>
   )
 }
 
--- Front-end/src/containers/HomePage/Medium.js
+++ Front-end/src/containers/HomePage/Medium.js
@@ -3,51 +3,15 @@ import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 import { BtnWhite } from '../../components/Buttons'
 import { routes } from '../../Router/router.js'
+import { MediumWrapper, MediumTitle, MediumSubTitle, ImageGrid } from './style'
 
-const Wrapper = styled.div`
-  width: 100%;
-  min-height: 900px;
-  background-color: #FFFFFF;
-  padding: 80px;
-  display:flex;
-  flex-direction:column;
-  align-items:center;
-  justify-content: space-evenly;
-`
-const Titulo = styled.h1`
-  font-size: 80px;
-  font-weight:900;
-  width: 70%;
-  text-align:center;
-  @media(max-width: 1200px){
-    font-size: 55px;
-  }
-`
-const SubTitle = styled.h3`
-  font-size: 30px;
-  font-weight:900;
-  width: 70%;
-  text-align:center;
-  @media(max-width: 1200px){
-    font-size: 20px;
-  }
-`
-const ImageGrid = styled.div`
-  width:70%;
-  display: grid;
-  grid-template-columns: repeat(4, 1fr);
-  column-gap: 20px;
-  img {
-    object-fit:cover;
-    width:100%;
-  }
-`
+
 function Medium() {
 
   return (
-    <Wrapper>
-      <Titulo>É música que você quer?</Titulo>
-      <SubTitle> Conheça os melhores lançamentos</SubTitle>
+    <MediumWrapper>
+      <MediumTitle>É música que você quer?</MediumTitle>
+      <MediumSubTitle> Conheça os melhores lançamentos</MediumSubTitle>
       <Link to={routes.allBands}><BtnWhite size='large'> VEJA NOSSAS BANDAS </BtnWhite></Link>
       <ImageGrid>
         <img src='https://picsum.photos/300/300' alt='Imagem1'></img>
@@ -55,7 +19,7 @@ function Medium() {
         <img src='https://picsum.photos/302/302' alt='Imagem1'></img>
         <img src='https://picsum.photos/299/299' alt='Imagem1'></img>
       </ImageGrid>
-    </Wrapper>
+    </MediumWrapper>
   )
 }
 
